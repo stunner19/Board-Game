c39e98e1f7bf20895137f2bd4e74fd675a57113d
Karanpal Singh <singhkaranpal81@gmail.com>
2020-01-13 23:57:11 +0530

    Adding Styles

--- src/Board.js
+++ src/Board.js
@@ -69,44 +69,61 @@ class Board extends Component {
         let board = this.state.board;
         let [y, x] = coord.split("-").map(Number);
 
-
-        function flipCell(y, x) {
-        // if this coord is actually on board, flip it
-
-        if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
-            board[y][x] = !board[y][x];
-        }
+        function flipCell(y, x) { 
+            if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
+                board[y][x] = !board[y][x];
+            }
         }
 
-        // TODO: flip this cell and the cells around it
+        flipCell(y,x);
+        flipCell(y,x - 1);
+        flipCell(y,x + 1);
+        flipCell(y - 1,x);
+        flipCell(y + 1,x);
 
-        // win when every cell is turned off
-        // TODO: determine is the game has been won
+        let hasWon = board.every(row => row.every(cell => !cell));
 
-        //this.setState({board, hasWon});
+        this.setState({ board : board, hasWon : hasWon});
     }
 
-
-    /** Render game board or winning message. */
-
     render() { 
+        if(this.state.hasWon){
+            return <h1>YOU WON !!!</h1>;
+        }
+
         let tableBoard = [];
         for(let y = 0; y < this.props.nrows; ++y){
             let row = [];
             for(let x = 0; x < this.props.ncols; ++x){
                 let coord = `${y}-${x}`
-                row.push(<Cell key = {coord} isLit = {this.state.board[y][x]} />)
+                row.push(<Cell key = {coord} isLit = {this.state.board[y][x]} flipCellsAround = {() => this.flipCellsAround(coord)} />)
          
            }
             tableBoard.push(<tr key = {y}>{row}</tr>)
         }
 
         return(
-            <table className = "Board">
-                <tbody>
-                    {tableBoard}
-                </tbody>
-            </table>
+            <div>
+                {this.state.hasWon ? (
+                        <div className = "winner">
+                            <span className = "neon-orange">YOU</span>    
+                            <span className = "neon-blue">WIN</span>                    
+                        </div>
+                    ) : (
+                        <div>
+                            <div className = "Board-title">
+                                <div className = "neon-orange">Lights</div>    
+                                <div className = "neon-blue">Out</div>                    
+                            </div>                   
+                            <table className = "Board">
+                                <tbody>
+                                    {tableBoard}
+                                </tbody>
+                            </table>
+                        </div>                
+                    )
+                }
+            </div>
         );
     }
 }
--- src/Cell.js
+++ src/Cell.js
@@ -1,20 +1,6 @@
 import React, {Component} from 'react'
 import "./Cell.css"
 
-
-/** A single cell on the board.
- *
- * This has no state --- just two props:
- *
- * - flipCellsAroundMe: a function rec'd from the board which flips this
- *      cell and the cells around of it
- *
- * - isLit: boolean, is this cell lit?
- *
- * This handles clicks --- by calling flipCellsAroundMe
- *
- **/
-
 class Cell extends Component {
   constructor(props) {
     super(props);
@@ -23,7 +9,7 @@ class Cell extends Component {
 
   handleClick(evt) {
     // call up to the board to flip cells around this cell
-    this.props.flipCellsAroundMe();
+    this.props.flipCellsAround();
   }
 
   render() {
